bdff0b6643d84c7edaff7d802f4e4bd5aa260e58
AshokMakunuri <ashokmakunuri@gmail.com>
2022-11-20 23:29:24 +0530

    [AshokMakunuri]-ArithmeticComputation

---
+++ UC3-compute3rdequation.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -x
+
+read -p "Enter the value of a " a
+read -p "Enter the value of b " b
+read -p "Enter the value of c " c
+result_1=$((($a+$b)*$c))
+result_2=$((($a*$b)+$c))
+result_3=$((($c+$a)/$b))
+echo "Result of (a+b*c)=" $result_1
+echo "Result of (a*b+c)=" $result_2
+echo "Result of (c+a/b)=" $result_3
